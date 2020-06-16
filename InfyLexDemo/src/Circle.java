c7d9c7cd9a5d39907ec9d6062c1a0ddc7ddeac8d
Muralidharan9402 <40212378+Muralidharan9402@users.noreply.github.com>
2020-06-16 15:44:52 +0530

    Update Circle.java

--- InfyLexDemo/src/Circle.java
+++ InfyLexDemo/src/Circle.java
@@ -6,7 +6,7 @@ public class Circle implements Shape {
 	@Override
 	public void draw() {
 		// TODO Auto-generated method stub
-		System.out.println("Drawing Circle:"+area*area);
+		System.out.println("No Use of Drawing Circle:"+area*area);
 	}
 
 }
